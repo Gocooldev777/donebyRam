62d279e5cfd32e22bdc58c78c90f786c468fbef0
Gocooldev777 <79393118+Gocooldev777@users.noreply.github.com>
2022-08-19 00:52:40 +0530

    Update rambapu.js

--- rambapu.js
+++ rambapu.js
@@ -1,5 +1,10 @@
 /* 
-Explanation
+Input:
+bill_number:1 A 1000 B 2000 C 1000
+bill_number:2 A 300 B 400 C 100 D 200
+bill_number:3 A 3000 B 100 D 200
+
+Explanation:
 bill_number:1 total=4000 average=1333.33 
 bill_number:2 total=1000 average=250
 bill_number:3 total=3300 average=1100
@@ -19,6 +24,7 @@ B remaining = -183.33
 C remaining = -483.33
 D remaining = -950.00
 
+Summary:
 D has to pay A : Rs.950 
 C has to pay A : Rs.483.33 
 B has to pay A : Rs.183.33 
@@ -27,26 +33,31 @@ B has to pay A : Rs.183.33
 //Input transactions
 state = {
     transactions : [
-        { bill_number:1,paid_by:'A',amount:1000 },
-        { bill_number:1,paid_by:'B',amount:2000 },
+        { bill_number:1,paid_by:'A',amount:2000 },
+        { bill_number:1,paid_by:'B',amount:200 },
         { bill_number:1,paid_by:'C',amount:1000 },
-        { bill_number:2,paid_by:'A',amount:300  },
-        { bill_number:2,paid_by:'B',amount:400  },
-        { bill_number:2,paid_by:'C',amount:100  },
-        { bill_number:2,paid_by:'D',amount:200  },
-        { bill_number:3,paid_by:'A',amount:3000 },
-        { bill_number:3,paid_by:'B',amount:100  },
-        { bill_number:3,paid_by:'D',amount:200  }
+        { bill_number:2,paid_by:'A',amount:1500  },
+        { bill_number:2,paid_by:'B',amount:0  },
+        { bill_number:2,paid_by:'C',amount:0  },
+        { bill_number:2,paid_by:'D',amount:1300  },
+        { bill_number:3,paid_by:'B',amount:1000  },
+        { bill_number:3,paid_by:'C',amount:0  },
+        { bill_number:4,paid_by:'X',amount:400 },
+        { bill_number:4,paid_by:'Y',amount:3000 },
+        { bill_number:4,paid_by:'Z',amount:400 },
+        { bill_number:5,paid_by:'E',amount:500 },
+        { bill_number:5,paid_by:'F',amount:700 },
+        { bill_number:5,paid_by:'G',amount:800 }
     ]
 }
 
 function init(){
-    print_transactions()
-    bill_average()
+    printTransactions()
+    billAverage()
 }
 
 //Printing the Input Transaction values
-function print_transactions(){
+function printTransactions(){
     console.log('----------------------------------------------------------')
     console.log('Input Transaction Values')
     for(let i=0;i<state.transactions.length;i++){
@@ -56,7 +67,7 @@ function print_transactions(){
 }
 
 //Finding average of each bill
-function bill_average(){
+function billAverage(){
     bill_average_list = []
     bill_number_list = []
     count = 0 
@@ -84,22 +95,22 @@ function bill_average(){
         bill_average_list.push(average)
     }
     //console.log(bill_average_list)
-    contribution_per_person()
+    contributionPerPerson()
 }
 
 //Finding the contribution of each person
-function contribution_per_person(){
-    people = find_people() 
+function contributionPerPerson(){
+    people = findPeople() 
     contribution_list = []
     for(let i=0;i<people.length;i++){
-        contribution_list.push(find_contribution(people[i]))
+        contribution_list.push(findContribution(people[i]))
     }
     //console.log(contribution_list)
-    amount_paid(contribution_list)
+    amountPaid(contribution_list)
 }
 
 //Finding all the persons who are involved
-function find_people(){
+function findPeople(){
     people = []
     for(let i=0;i<state.transactions.length;i++)
     {
@@ -111,7 +122,7 @@ function find_people(){
 }
 
 //Finding the contribution of a person in ever bill
-function find_contribution(person){ 
+function findContribution(person){ 
     contribution = 0 
     for(let i=0;i<state.transactions.length;i++)
     {
@@ -130,8 +141,8 @@ function find_contribution(person){
 }
 
 //Finding the total amount paid by ach person
-function amount_paid(contribution_list){
-    people = find_people()
+function amountPaid(contribution_list){
+    people = findPeople()
     amount_paid_list = []
     for(let i=0;i<people.length;i++)
     {
@@ -146,11 +157,11 @@ function amount_paid(contribution_list){
         amount_paid_list.push(total_amount_paid) 
     }
     //console.log(amount_paid_list)
-    remaining_amount(amount_paid_list,contribution_list)
+    remainingAmount(amount_paid_list,contribution_list)
 }
 
 //Finding the amount to be owed by each person
-function remaining_amount(amount_paid_list,contribution_list){
+function remainingAmount(amount_paid_list,contribution_list){
     remaining_list = []
     for(let i =0;i<amount_paid_list.length;i++){
         remaining_list.push(amount_paid_list[i]-contribution_list[i]) 
@@ -161,7 +172,7 @@ function remaining_amount(amount_paid_list,contribution_list){
 
 //Settlement by each person
 function settlement(remaining_list){
-    people = find_people()
+    people = findPeople()
     person_object = {}
     for(let i=0;i<remaining_list.length;i++){
         person_object[people[i]] = remaining_list[i]
@@ -169,66 +180,66 @@ function settlement(remaining_list){
     console.log('Amount paid/owed by each person : ')
     console.log(person_object)
     console.log('----------------------------------------------------------')
-    positive_arr = []
-    negative_arr = [] 
-    positive_values = {}
-    negative_values = {}
+    person_to_get_array = []
+    person_to_pay_array = [] 
+    person_to_get = {}
+    person_to_pay = {}
 
     //Sorting the positive and negative remaining amounts 
     for (key in person_object){
         if(person_object[key]>0)
-            positive_arr.push(person_object[key])
+            person_to_get_array.push(person_object[key])
         else
-            negative_arr.push(person_object[key])
+            person_to_pay_array.push(person_object[key])
     }
 
     function sortNum(a,b){
         return a-b
     }
-    positive_arr.sort(sortNum).reverse()
-    negative_arr.sort(sortNum)
+    person_to_get_array.sort(sortNum).reverse()
+    person_to_pay_array.sort(sortNum)
 
     //Separate objects for positive and negative values
-    for(i of positive_arr){
+    for(i of person_to_get_array){
         for(j in person_object){
             if(i == person_object[j]){
-                positive_values[j] = i
+                person_to_get[j] = i
             }
         }
     }
 
-    for(i of negative_arr){
+    for(i of person_to_pay_array){
         for(j in person_object){
             if(i == person_object[j]){
-                negative_values[j] = i
+                person_to_pay[j] = i
             }
         }
     }
 
     //Printing the Summary
     console.log('SUMMARY : ')
-    for(i in positive_values){ 
-        for(j in negative_values){
-            if(positive_values[i] != 0 & negative_values[j] != 0){
-                if(positive_values[i]<Math.abs(negative_values[j]))
+    for(i in person_to_get){ 
+        for(j in person_to_pay){
+            if(Math.round(Math.abs(person_to_get[i])) != 0 & Math.round(Math.abs(person_to_pay[j])) != 0){
+                if(person_to_get[i]<Math.abs(person_to_pay[j]))
                 {
-                    console.log(`${j} has to pay ${i} : Rs.${positive_values[i].toFixed(2)} `)
-                    remaining = negative_values[j] + positive_values[i]
-                    negative_values[j] = remaining
-                    positive_values[i] = 0
+                    console.log(`${j} has to pay ${i} : Rs.${person_to_get[i].toFixed(2)} `)
+                    remaining = person_to_pay[j] + person_to_get[i]
+                    person_to_pay[j] = remaining
+                    person_to_get[i] = 0
                 }
-                else if(positive_values[i]>Math.abs(negative_values[j]))
+                else if(person_to_get[i]>Math.abs(person_to_pay[j]))
                 {
-                    console.log(`${j} has to pay ${i} : Rs.${Math.abs(negative_values[j].toFixed(2))} `)
-                    remaining = negative_values[j] + positive_values[i]
-                    negative_values[j] = 0
-                    positive_values[i] = remaining
+                    console.log(`${j} has to pay ${i} : Rs.${Math.abs(person_to_pay[j].toFixed(2))} `)
+                    remaining = person_to_pay[j] + person_to_get[i]
+                    person_to_pay[j] = 0
+                    person_to_get[i] = remaining
                 }
-                else if(positive_values[i] == Math.abs(negative_values[j]))
+                else if(person_to_get[i] == Math.abs(person_to_pay[j]))
                 {
-                    console.log(`${j} has to pay ${i} : Rs.${positive_values[i].toFixed(2)} `)
-                    positive_values[i] = 0
-                    negative_values[j] = 0             
+                    console.log(`${j} has to pay ${i} : Rs.${person_to_get[i].toFixed(2)} `)
+                    person_to_get[i] = 0
+                    person_to_pay[j] = 0             
                 }
                 else
                     break
